189fee3ed9de38b1aab5a91d7d355a5a6e0ae76c
Monkey <371816216@qq.com>
2017-08-18 01:28:51 +0800

    update

--- app_websocket/mongo.js
+++ app_websocket/mongo.js
@@ -1,4 +1,6 @@
 var mongo_client = require('mongodb').MongoClient
 var DB_CONN_STR = 'mongodb://localhost:27017/test'
 
-mongo_client.connect(DB_CONN_STR,function(err,db){});
+mongo_client.connect(DB_CONN_STR,function(err,db){
+    
+});
